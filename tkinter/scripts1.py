55a5f09b4c1675280f6805f269b52eb971148e87
vabigdatamover <vabigdatamover@gmail.com>
2020-08-08 15:19:55 -0400

    working files

--- tkinter/scripts1.py
+++ tkinter/scripts1.py
@@ -2,7 +2,19 @@ from tkinter import *
 
 window=Tk()
 
-b1=Button(window,text="Execute")
+#function
+def km_miles():
+    miles=float(e1_value.get())*1.606
+    t1.insert(END, miles)
+
+b1=Button(window,text="Execute", command=km_miles)
 b1.grid(row=0, column=0)
+
+e1_value=StringVar()
+e1=Entry(window, textvariable=e1_value)
+e1.grid(row=0,column=1)
+
+t1=Text(window, height=1, width=20)
+t1.grid(row=0,column=2)
 
 window.mainloop()
