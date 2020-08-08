f1031f58c5a9dfcf7d649b66858450de97a917ac
vabigdatamover <vabigdatamover@gmail.com>
2020-08-07 21:36:16 -0400

    working notebook

--- Mapping.py
+++ Mapping.py
@@ -1,6 +1,10 @@
 import folium
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
-map.add_child(folium.Marker())
+fg = folium.FeatureGroup(name="My Map")
 
-map.save("Map2.html")
+for coordinates in [[38.2, -99.1], [38.3, -99.2]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a marker", icon=folium.Icon(color='green')))
+map.add_child(fg)
+
+map.save("Map1.html")
